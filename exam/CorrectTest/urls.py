3ffce7a0a729b65ff74a642bdbe931373681b8ff
jjjjssss6 <74179995+jjjjssss6@users.noreply.github.com>
2024-05-04 00:06:48 +0800

    Update urls.py

--- exam/CorrectTest/urls.py
+++ exam/CorrectTest/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('get_correct_ques', CorrectTest.views.GetCorrectQues),
     path('submit_judge_mission', CorrectTest.views.SubmitJudgeMission),
     path('get_judge_result', CorrectTest.views.GetJudgeResult),
+    path('get_grade_table', CorrectTest.views.GetGradeTable),
 ]
